9eebb1665012162618be16571d790476b2ccefaf
NikkoriSa <saika764@outlook.jp>
2024-06-21 10:15:26 +0900

    First CommitGogogo
    Attenction please

---
+++ all計算.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+a_i = int(a)
+b_i = int(b)
+
+
+print('和'+str(a_i+b_i))
+print('差'+str(a_i-b_i))
+print('乗'+str(a_i*b_i))
+print('商'+str(a_i/b_i))
+print('あまり'+str(a_i%b_i))
+
